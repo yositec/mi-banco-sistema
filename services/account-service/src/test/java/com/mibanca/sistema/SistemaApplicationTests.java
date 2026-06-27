a12cea28595a29f7790ce3c888ecc534f43a0220
Yosemar Castillo Valderrama <yojecava@outlook.com>
2026-06-27 10:17:26 -0600

    fix: disable r2dbc autoconfiguration in tests

--- services/account-service/src/test/java/com/mibanca/sistema/SistemaApplicationTests.java
+++ services/account-service/src/test/java/com/mibanca/sistema/SistemaApplicationTests.java
@@ -3,9 +3,12 @@ package com.mibanca.sistema;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@EnableAutoConfiguration(exclude = {R2dbcAutoConfiguration.class})
 class SistemaApplicationTests {
 
 	@Test
