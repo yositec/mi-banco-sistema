1bf3d28bedb468de7bedac15011426690b980a37
Yosemar Castillo Valderrama <yojecava@outlook.com>
2026-06-27 10:10:47 -0600

    fix: add missing import for ActiveProfiles

--- services/account-service/src/test/java/com/mibanca/sistema/SistemaApplicationTests.java
+++ services/account-service/src/test/java/com/mibanca/sistema/SistemaApplicationTests.java
@@ -2,6 +2,7 @@ package com.mibanca.sistema;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
